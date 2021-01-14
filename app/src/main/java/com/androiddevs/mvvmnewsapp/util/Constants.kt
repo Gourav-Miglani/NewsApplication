fb3f430e6d449908491f4f64331b6d7d0d9cfdb8
Gourav Miglani <gouravmiglani1811@gmail.com>
2021-01-14 12:44:20 +0530

    Implementation of Kotlin Coroutines,MVVM,REST API,Room Library in a single application for getting news from different sources

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "91919a2442d14306aa6bcda2d64e14aa"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
